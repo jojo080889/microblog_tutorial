fa45695531253aa5652152c3aa0edd77d5e4e757
jojo080889 <jojo080889@gmail.com>
2011-06-15 10:10:29 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 MicroblogTutorial::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
+  match '/signup', :to => 'users#new'
 
-  get "pages/about"
-
-  get "pages/help"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
+  #         given url      => controllername#actionname
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
